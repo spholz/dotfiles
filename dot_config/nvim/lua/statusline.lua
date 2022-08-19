90fc3fee07ce802d6ceae819c9efe2d3257f57ef
Sönke Holz <sholz8530@gmail.com>
2022-08-19 18:03:58 +0200

    Update dot_config/nvim/lua/statusline.lua

--- dot_config/nvim/lua/statusline.lua
+++ dot_config/nvim/lua/statusline.lua
@@ -21,9 +21,9 @@ lualine.setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch', 'diff'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'diagnostics', 'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
