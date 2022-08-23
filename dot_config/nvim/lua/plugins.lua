0c58ac34c97a8c39c2ee57bf90a015d03f7a1145
Sönke Holz <sholz8530@gmail.com>
2022-08-23 16:58:53 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -218,6 +218,7 @@ return function(bootstrap)
 
         use {
             'jose-elias-alvarez/null-ls.nvim',
+            requires = 'nvim-lua/plenary.nvim',
             config = function()
                 local null_ls = require 'null-ls'
                 null_ls.setup {
