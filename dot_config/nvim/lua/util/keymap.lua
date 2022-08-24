ad21816a810ea5bcbc539b97eda9634d016e0dc1
Sönke Holz <sholz8530@gmail.com>
2022-08-24 19:49:33 +0200

    Add dot_config/nvim/lua/util/keymap.lua

---
+++ dot_config/nvim/lua/util/keymap.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+---@param mode string|table
+---@param lhs string
+---@param rhs string|function
+---@param opts_ table
+---@param description string
+M.map_with_desc = function (mode, lhs, rhs, opts_, description)
+    vim.keymap.set(mode, lhs, rhs, vim.tbl_extend('error', opts_, { desc = description }))
+end
+
+return M
