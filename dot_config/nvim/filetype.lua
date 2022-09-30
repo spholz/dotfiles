29671e2bdc3b02aa0e5ff687e39886bda2233943
Sönke Holz <sholz8530@gmail.com>
2022-09-30 23:33:08 +0200

    Update dot_config/nvim/filetype.lua

--- dot_config/nvim/filetype.lua
+++ dot_config/nvim/filetype.lua
@@ -1,13 +1,10 @@
 vim.filetype.add {
     extension = {
-        -- glsl (OpenGL)
         vert = 'glsl',
         frag = 'glsl',
 
-        -- wgsl (WebGPU)
         wgsl = 'wgsl',
 
-        -- qml (Qt Quick)
         qml = 'qmljs',
     },
 }
