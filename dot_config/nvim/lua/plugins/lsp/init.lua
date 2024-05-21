67cac5fbd39c1cbf82c27ac534c6ea17dafcfca0
Sönke Holz <sholz8530@gmail.com>
2024-05-21 12:18:05 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp/init.lua

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -50,7 +50,6 @@ return {
             }
 
             local external_servers = {
-                ghdl_ls = {}, -- part of pyghdl
                 gdscript = {
                     -- server integrated into the editor
                     cmd = vim.lsp.rpc.connect('127.0.0.1', '6005'), -- godot 4 has different port
