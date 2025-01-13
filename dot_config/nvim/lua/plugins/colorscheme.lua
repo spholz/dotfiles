6e80594569ac89dabf9519048e59a0200a6ee1fb
Sönke Holz <sholz8530@gmail.com>
2025-01-13 23:23:09 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -40,12 +40,18 @@ return {
                         strikethrough = true,
                     })
 
+                    vim.api.nvim_set_hl(0, 'NonText', { link = 'Comment' })
+
+                    vim.api.nvim_set_hl(0, 'Added', { fg = '#a6e22e', bg = '#232526' })
+                    vim.api.nvim_set_hl(0, 'Changed', { fg = '#66d9ef', bg = '#232526' })
+                    vim.api.nvim_set_hl(0, 'Removed', { fg = '#f92672', bg = '#232526' })
+
                     -- fixes for nvim 0.10
                     vim.api.nvim_set_hl(0, '@variable', { link = 'Identifier' })
                     vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Pmenu' })
                     vim.api.nvim_set_hl(0, 'IncSearch', { link = 'Search' })
-                    -- vim.api.nvim_set_hl(0, 'DiagnosticError', { link = 'ErrorMsg' })
-                    vim.api.nvim_set_hl(0, 'NonText', { link = 'Comment' })
+                    vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = '#f92672' })
+                    vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = '#e6db74' })
                 end,
                 group = vim.api.nvim_create_augroup('color_scheme_fix', {}),
             })
